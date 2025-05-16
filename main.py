c494e49bf7c57987e3e67bdf7079398257be5287
PhillipeMarq <phillipemarques22@gmail.com>
2025-05-15 23:05:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import datetime
-from utils.analyzer import analisar_jogos
-from utils.telegram import enviar_mensagem
-
-def main():
-    hoje = datetime.date.today()
-    dias = [hoje + datetime.timedelta(days=i) for i in range(4)]
-    for dia in dias:
-        jogos = analisar_jogos(dia)
-        for jogo in jogos:
-            enviar_mensagem(jogo)
+from telegrama import iniciar_bot
 
 if __name__ == "__main__":
-    main()
+    iniciar_bot()
