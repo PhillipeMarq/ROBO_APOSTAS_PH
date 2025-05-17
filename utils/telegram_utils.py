43a79f59f367523e013b37122eb548592d975d93
PhillipeMarq <phillipemarques22@gmail.com>
2025-05-16 22:27:31 -0300

    Create telegram_utils.py

---
+++ utils/telegram_utils.py
@@ -0,0 +1,26 @@
+--- INÍCIO DO ARQUIVO: utils/telegram_utils.py ---
+from telegram import Bot
+
+def enviar_mensagem(bot: Bot, texto: str):
+    bot.send_message(chat_id="SEU_CHAT_ID", text=texto)  # SEU CHAT ID
+
+def handle_comandos(update: dict, bot: Bot):
+    message = update.get("message", {})
+    text = message.get("text", "")
+    chat_id = message.get("chat", {}).get("id")
+
+    if "/analise " in text:
+        jogo = text.replace("/analise ", "")
+        from utils.analysis import analisar_jogo_individual
+        resposta = analisar_jogo_individual(jogo)
+        bot.send_message(chat_id=chat_id, text=resposta)
+    elif text == "/analise":
+        from utils.analysis import analisar_jogos
+        respostas = analisar_jogos()
+        for r in respostas:
+            bot.send_message(chat_id=chat_id, text=r)
+    else:
+        bot.send_message(chat_id=chat_id, text="Comando não reconhecido.")
+
+from analysis import analisar_jogos, analisar_jogo_individual
+--- FIM DO ARQUIVO: utils/telegram_utils.py ---
